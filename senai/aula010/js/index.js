fe610c9a6ca8225c6cc7869800abafab2dab411d
isaacgoncod <isaacgoncod@gmail.com>
2023-04-10 20:48:08 -0300

    aula010

--- senai/aula010/js/index.js
+++ senai/aula010/js/index.js
@@ -6,23 +6,28 @@ const api = axios.create({
 });
 
 function getTemp() {
-  api
-    .get(
-      `/current.json?key=0caff844f67f4ae4adc115813231004&q=${inputCidade.value}&lang=pt`
-    )
-    .then((resp) => {
-      const city = resp.data.location.name;
-      const tempC = resp.data.current.temp_c;
-      const feelslikeC = resp.data.current.feelslike_c;
-      const descTemp = resp.data.current.condition.text;
-      const iconTemp = resp.data.current.condition.icon;
+  if (inputCidade.value == "") {
+    alert(`Preencha o campo necessário`);
+  } else {
+    api
+      .get(
+        `/current.json?key=0caff844f67f4ae4adc115813231004&q=${inputCidade.value}&lang=pt`
+      )
+      .then((resp) => {
+        const city = resp.data.location.name;
+        const tempC = resp.data.current.temp_c;
+        const feelslikeC = resp.data.current.feelslike_c;
+        const descTemp = resp.data.current.condition.text;
+        const iconTemp = resp.data.current.condition.icon;
 
-      info.innerHTML = `<p>${city}</p>
+        info.innerHTML = `
+      <img src=${iconTemp} /><p>${city}</p>
       <p>Temperatura(°C): ${tempC}</p>
       <p>Sens.Térmica(°C): ${feelslikeC}</p>
       <p>Descrição: ${descTemp}</p>
-      <img src=${iconTemp} />`;
-    });
+      `;
+      });
 
-  info.style.visibility = "visible";
+    info.style.visibility = "visible";
+  }
 }
